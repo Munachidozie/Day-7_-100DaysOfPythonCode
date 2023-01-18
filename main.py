db72aeaf98adb19957a54088eb823c5a245e341e
Munachimso Ukaoha <65025073+Munachidozie@users.noreply.github.com>
2023-01-18 22:48:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+print ("== Fake Fan Detector ==")
+print (" ")
+cartoon = input ("What's your favorite cartoon? ")
+if cartoon == "american dad" or "American Dad" or "American dad":
+  print ("Oh really?!!!")
+  main_character = input ("Well then, tell me the full name of the Dad in question? ")
+  if main_character == "stan smith" or "Stan Smith":
+    print ("That was an easy one")
+  else:
+    print ("Wrong. You're a fake")
+    job = input ("What's his job then? I bet you don't know that one. He's a")
+    if job == "CIA Agent":
+      print ("Okay")
+    else:
+      print ("Close but no. You're just a usual viewer, not a true fan.")
+      dress = input ("what is the colour of his wife's dress?")
+      if dress == "pink":
+        print ("Well, I see your a man of culture")
+      else:
+        print ("Wrong. You came close but still have a long way to go.")
+        
+elif cartoon == "the simpsons" or "The Simpsons" or "the Simpsons" or "The simpsons":
+  print ("Oh really?")
+  naughty = input ("What's the first name of prankster and naughty one among the Simpsons")
+  if naughty == "Bart" or "bart":
+    print ("That was an easy one")
+  else:
+    print ("Wrong. You're a fake")
+    name = input ("What is the name of the baby? ")
+    if name == "Maggie" or "maggie":
+      print ("Okay")
+    else:
+      print ("Close but no. You're just a usual viewer, not a true fan.")
+      car = input ("What is the color of the family car? ")
+      if car == "pink" or "Pink":
+        print ("Well, I see your a man of culture")
+      else:
+        print ("Wrong. You came close but still have a long way to go.")
+
+else:
+  print ("well............it's okay")
